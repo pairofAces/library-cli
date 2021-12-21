7163f71822e84a71ba13474ca9ed32dc2533c15a
pairofAces <karanchauhan16@gmail.com>
2021-12-20 23:50:57 -0500

    created initial menu for testing

---
+++ hello.py
@@ -0,0 +1,10 @@
+# create function to print general menu
+def print_menu():
+    print("""Choose an option:
+    1. print all books
+    2. add a book
+    3. update a book
+    4. delete a book
+    """)
+
+print_menu()
