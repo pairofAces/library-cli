edc1afae2ffdf9d1d6c90c5a3961549676de78c9
pairofAces <karanchauhan16@gmail.com>
2021-12-23 22:33:16 -0500

    generating booksSDK

--- booksSDK.py
+++ booksSDK.py
@@ -1,2 +1,12 @@
 import sqlite3
 import book from Book
+import os.path
+
+l = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+conn = sqlite3.connect(os.path.join(l, 'books.db'))
+
+c = conn.cursor()
+
