8079bd4f4e2eccba3d110a6c5773cb61872a5a18
pairofAces <karanchauhan16@gmail.com>
2021-12-27 23:51:41 -0500

    created cursor method and initial connection to database

--- booksSDK.py
+++ booksSDK.py
@@ -10,3 +10,12 @@ conn = sqlite3.connect(os.path.join(l, 'books.db'))
 
 c = conn.cursor()
 
+# create the table
+c.execute('''CREATE TABLE IF NOT EXISTS books
+              (title TEXT, pages INTEGER)''')
+
+def cursor():
+
+    l = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    return sqlite3.connect(os.path.join(l, 'books.db')).cursor()
